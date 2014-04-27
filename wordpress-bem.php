760f737ed5885adf23954b0e260b6e685b55722a
Sam Hastings <sam@decodehq.co.uk>
2014-04-27 22:26:23 +0100

    Fixed WordPress plugin headers. Moved core code to separate file to enable PHP version checking in mail plugin script.

--- wordpress-bem.php
+++ wordpress-bem.php
@@ -1,16 +1,14 @@
 <?php
 
-/**
- * Plugin Name: WordPress BEM
- * Plugin URI:  http://github.com/decodedigital/wordpress-bem
- * Description: A plugin that hooks into natively generated WordPress template code and converts class names to block__element--modifier (BEM) notation.
- * Version:     0.1
- * Author:      Sam Hastings
- * Author URI:  http://decodehq.co.uk
- * License:     GPL v3
- */
-
 /*
+Plugin Name: WordPress BEM
+Plugin URI:  http://github.com/decodedigital/wordpress-bem
+Description: A plugin that hooks into natively generated WordPress template code and converts class names to block__element--modifier (BEM) notation.
+Version:     0.1
+Author:      Sam Hastings
+Author URI:  http://decodehq.co.uk
+License:     GPL v3
+
 WordPress BEM Plugin
 Copyright (C) 2013, Sam Hastings - sam@decodehq.co.uk
 
@@ -28,192 +26,19 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-add_filter('nav_menu_css_class', function($classes, $item, $args) {
+define('WPBEM_MIN_PHP_VERSION', '5.3.0');
 
-	$root = sprintf('%s__item', isset($args->wpbem_block) ? $args->wpbem_block : $args->menu_class);
+if(!version_compare(PHP_VERSION, WPBEM_MIN_PHP_VERSION, '>=')) {
 
-	$classes = array();
+	deactivate_plugins(plugin_basename(__FILE__));
 
-	$classes[] = $root;
-	$classes[] = sprintf('%s--%s', $root, $item->ID);
-	$classes[] = sprintf('%s--%s', $root, $item->object);
-
-	if($item->current) {
-		$classes[] = sprintf('%s--current', $root);
-	}
-
-	if($item->current_item_ancestor) {
-		$classes[] = sprintf('%s--ancestor', $root);
-	}
-
-	if($item->current_item_parent) {
-		$classes[] = sprintf('%s--parent', $root);
-	}
-	
-	if(in_array('menu-item-has-children', $item->classes)) {
-		$classes[] = sprintf('%s--has-children', $root);
-	}
-	
-	if(in_array('menu-item-home', $item->classes)) {
-		$classes[] = sprintf('%s--home', $root);
-	}
-
-	return $classes;
-
-}, 10, 3);
-
-add_filter('page_css_class', function($class, $page, $depth, $args, $current_page) {
-
-	$root = sprintf('%s__item', isset($args['wpbem_block']) ? $args['wpbem_block'] : $args['menu_class']);
-
-	$classes = array();
-
-	$classes[] = $root;
-	$classes[] = sprintf('%s--%s', $root, $page->ID);
-	$classes[] = sprintf('%s--%s', $root, $depth);
-
-	if($current_page == $page->ID) {
-		$classes[] = sprintf('%s--current', $root);
-	}
-	
-	if(in_array('page_item_has_children', $class)) {
-		$classes[] = sprintf('%s--has-children', $root);
-	}
-
-	return $classes;
-	
-}, 10, 5);
-
-add_filter('body_class', function($classes) {
-
-	$block = apply_filters('wpbem_body_block', 'body');
-
-	$classes = array_map(function($class) use($block) {
-		return sprintf('%s--%s', $block, $class);
-	}, $classes);
-
-	return $classes;
-
-}, 10, 1);
-
-add_filter('post_class', function($classes) {
-
-	$block = apply_filters('wpbem_post_block', 'post');
-
-	$classes = array_map(function($class) use($block) {
-
-		if('post' == $class) {
-			return $block;
-		}
-
-		if('post-' == substr($class, 0, 5)) {
-			$class = substr($class, 5);
-		}
-
-		return sprintf('%s--%s', $block, $class);
-
-	}, $classes);
-
-	return $classes;
-
-}, 10, 1);
-
-if(apply_filters('wpbem_amend_comment_form', true)) {
-
-	add_action('comment_form_before', function() {
-		ob_start();
-	});
-	
-	add_action('comment_form_after', function() {
-	
-		$container_class = apply_filters('wpbem_comment_container_block', 'comments');
-		$form_class      = apply_filters('wpbem_comment_form_block',      'comment-form');
-	
-		$form = ob_get_contents();
-		ob_end_clean();
-	
-		$dom = new DomDocument;
-		$dom->preserveWhiteSpace = false;
-		$dom->formatOutput = false;
-	
-		$dom->loadHTML($form);
-	
-		$root = $dom->getElementById('respond');
-		$root->setAttribute('class', $container_class);
-	
-		$title = $dom->getElementById('reply-title');
-		$title->setAttribute('class', sprintf('%s__title', $container_class));
-		$title->getElementsByTagName('a')->item(0)->setAttribute('class', sprintf('%s__cancel-link', $container_class));
-	
-		$form = $dom->getElementById('commentform');
-		$form->setAttribute('class', $form_class);
-	
-		foreach($form->getElementsByTagName('p') as $p) {
-	
-			$current_class = $p->getAttribute('class');
-	
-			if('comment-form-' == substr($current_class, 0, 13)) {
-				$p->setAttribute('class', sprintf('%1$s__row  %1$s__row--%2$s', $form_class, substr($current_class, 13)));
-			}
-	
-		}
-	
-		foreach($form->getElementsByTagName('label') as $label) {
-			$label->setAttribute('class', sprintf('%s__label', $form_class));
-		}
-	
-		foreach($form->getElementsByTagName('span') as $span) {
-			if('required' == $span->getAttribute('class')) {
-				$span->setAttribute('class', sprintf('%s__required', $form_class));
-			}
-		}
-	
-		foreach($form->getElementsByTagName('input') as $input) {
-	
-			$input_classes = array();
-	
-			switch($input->getAttribute('type')) {
-				case 'hidden':
-					break;
-				case 'email':
-					$input_classes[] = sprintf('%s__input', $form_class);
-					$input_classes[] = sprintf('%s__input--email', $form_class);
-					break;
-				case 'url':
-					$input_classes[] = sprintf('%s__input', $form_class);
-					$input_classes[] = sprintf('%s__input--url', $form_class);
-					break;
-				case 'submit':
-					$input_classes[] = sprintf('%s__button', $form_class);
-					$input_classes[] = sprintf('%s__button--submit', $form_class);
-					break;
-				case 'reset':
-					$input_classes[] = sprintf('%s__button', $form_class);
-					$input_classes[] = sprintf('%s__button--reset', $form_class);
-					break;
-				case 'button':
-					$input_classes[] = sprintf('%s__button', $form_class);
-					break;
-				default:
-					$input_classes[] = sprintf('%s__input', $form_class);
-					$input_classes[] = sprintf('%s__input--text', $form_class);
-					break;
-			}
-	
-			$input->setAttribute('class', implode(' ', $input_classes));
-	
-		}
-	
-		if($textarea = $dom->getElementById('comment')) {
-			$textarea->setAttribute('class', implode(' ', array(
-				sprintf('%s__textarea', $form_class),
-				sprintf('%s__input--textarea', $form_class),
-				sprintf('%s__comments-box', $form_class),
-			)));
-		}
-	
-		echo $dom->saveHTML($root);
-	
-	});
+	wp_die(sprintf(
+		'<h1>Unable to activate plugin</h1><p>BEM for WordPress requires PHP version %s or later. You are currently running version %s.</p><p>This plugin has been deactivated.</p><a href="%s" class="button  button-large">Return to the plugins page</a>',
+		WPBEM_MIN_PHP_VERSION,
+		PHP_VERSION,
+		esc_url(get_admin_url(null, 'plugins.php'))
+	));
 
 }
+
+require_once 'hooks.php';
